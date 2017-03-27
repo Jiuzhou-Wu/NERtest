e482eaafb633eccca83d60d94ef08ff463a9e71d
Jiuzhou <nova2357@gmail.com>
2017-03-27 18:18:50 -0400

    posTagger doest work

--- NERtest/src/TaggerDemo.java
+++ NERtest/src/TaggerDemo.java
@@ -2,7 +2,7 @@ import edu.stanford.nlp.tagger.maxent.MaxentTagger;
  //treetagger
 public class TaggerDemo {
     public static void main(String[] args){
-    	MaxentTagger tagger = new MaxentTagger("lib\\taggers\\english-bidirectional-distsim.tagger");
+    	MaxentTagger tagger = new MaxentTagger("lib\\models\\english-left3words-distsim.tagger");
     	String test = "I like this apple. How about you?";
     	System.out.println(tagger.tagString(test));
     }
