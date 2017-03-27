ebb9eac80c7dbeaf0f9475e3d26df89a6e06814c
Jiuzhou <nova2357@gmail.com>
2017-03-27 17:58:13 -0400

    Test tagger

--- NERtest/src/TaggerDemo.java
+++ NERtest/src/TaggerDemo.java
@@ -1,18 +1,9 @@
-import java.io.IOException;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
- 
+ //treetagger
 public class TaggerDemo {
     public static void main(String[] args){
- 
-        // Initialize the tagger
-        MaxentTagger tagger = new MaxentTagger("lib\\taggers\\english-caseless-left3words-distsim.tagger");
-        // The sample string
-        String sample = "This is a sample text";
- 
-        // The tagged string
-        String tagged = tagger.tagString(sample);
- 
-        // Output the result
-        System.out.println(tagged);
+    	MaxentTagger tagger = new MaxentTagger("lib\\taggers\\english-bidirectional-distsim.tagger");
+    	String test = "I like this apple. How about you?";
+    	System.out.println(tagger.tagString(test));
     }
 }
